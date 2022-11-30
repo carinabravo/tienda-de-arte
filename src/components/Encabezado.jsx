70f07a3b936fc5ac2cae0b6c09cc691d7cb87aad
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-11-30 19:03:15 -0300

    se modifica frase del encabezado

--- src/components/Encabezado.jsx
+++ src/components/Encabezado.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 
 const Encabezado = () => {
+
     return (
         <div className="container-fluid">
             <div className="row align-items-center mb-2 bg-dark">
                 <div className="col-md-12 d-flex justify-content-center">
-                    <p className="mt-2" style={{ color: "white" }}>TIENDA DE ARTE -  DANNA BLENK</p>
+                    <p className="mt-2" style={{ color: "white" }}>TIENDA ONLINE DE ARTE ABSTRACTO</p>
                 </div>
             </div>
         </div>
