3c6d90b780d2ace5950630b4a70f4c9a8c0330a9
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-04 21:15:04 -0300

    se agrega link al carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CartWidget = () => {
 
     return (
         <button type="button" className="btn position-relative">
-            <img src="images/logo/cart4.svg" alt="Carrito" width="28" />
-            <span className="position-absolute start-5 translate-middle badge rounded-pill bg-danger">1</span>
+            <Link to={"/carrito"}>
+                <img src={"images/logo e iconos/cart4.svg"} alt="Carrito" width="28" />
+                <span className="position-absolute start-5 translate-middle badge rounded-pill bg-danger">1</span>
+            </Link>
         </button>
     )
 }
