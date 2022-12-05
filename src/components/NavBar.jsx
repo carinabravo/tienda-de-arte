672f4593fb77363a9308504d245b487e5fed2fc3
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-04 21:30:10 -0300

    se agregan navlinks y links e icons + ajustes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,44 +1,49 @@
-import React from "react";
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import CartWidget from './CartWidget';
 
-import CartWidget from "./CartWidget";
 
 const NavBar = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-11 mt-3 mb-3">
+                <div className="col-md-10 mt-5 mb-5">
                     <nav className="navbar navbar-expand-lg">
                         <div className="container">
-                            <a className="navbar-brand" href="#p"><img src="images/logo/logo.jpg" alt="logo" width="350" /></a>
+                            <Link className="navbar-brand" to={"/"}><img src={"images/logo e iconos/logo.jpg"} alt="logo" width={350} /></Link>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse mx-3" id="navbarNav">
                                 <ul className="navbar-nav me-3">
                                     <li className="nav-item">
-                                        <a className="nav-link" href="/inicio">Inicio</a>
+                                        <NavLink className="nav-link" activeclassname="page" to={"/"}>Inicio</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" activeclassname="page" to={"/biografia"}>Biografía</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" activeclassname="page" to={"/contacto"}>Contacto</NavLink>
                                     </li>
                                     <li className="nav-item dropdown me-5">
                                         <a className="nav-link dropdown-toggle" href="#p" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Categorías</a>
+                                            Obras</a>
                                         <ul className="dropdown-menu">
-                                            <li><a className="dropdown-item" href="#p">Tienda</a></li>
-                                            <li><a className="dropdown-item" href="#p">Promos</a></li>
-                                            <li><a className="dropdown-item" href="#p">Contacto</a></li>
-                                            <li><hr class="dropdown-divider" /></li>
-                                            <li><a className="dropdown-item" href="#p">Biografía de Danna</a></li>
+                                            <NavLink className="dropdown-item" activeclassname="page" to={"/category/acrilico"}><img src={"images/logo e iconos/palette.svg"} className="me-2" alt="palette" width="18" />Acrílicos</NavLink>
+                                            <hr className="dropdown-divider" />
+                                            <NavLink className="dropdown-item" activeclassname="page" to={"/category/acuarela"}><img src={"images/logo e iconos/palette.svg"} className="me-2" alt="palette" width="18" />Acuarelas</NavLink>
+                                            <hr className="dropdown-divider" />
+                                            <NavLink className="dropdown-item" activeclassname="page" to={"/category/oleo"}><img src={"images/logo e iconos/palette.svg"} className="me-2" alt="palette" width="18" />Óleos</NavLink>
+                                            <hr className="dropdown-divider" />
+                                            <NavLink className="dropdown-item" activeclassname="page" to={"/category/tecnica-mixta"}><img src={"images/logo e iconos/palette.svg"} className="me-2" alt="palette" width="18" />Técnica mixta</NavLink>
                                         </ul>
                                     </li>
                                 </ul>
-                                <form className="d-flex" role="search">
-                                    <input className="form-control me-2" type="search" placeholder="Obra..." aria-label="Search" />
-                                    <button className="btn btn-outline-success" type="Search">Buscar</button>
-                                </form>
                             </div>
                         </div>
                     </nav>
                 </div>
-                <div className="col-md-1 d-flex align-items-center justify-content-end">
+                <div className="col-md-2 d-flex align-items-center justify-content-end">
                     <CartWidget />
                 </div>
             </div>
