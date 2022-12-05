eecc1d8c35e928e8858a72a374c33e5b2adb7361
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-04 21:13:49 -0300

    se hacen ajustes de texto e imagen al carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './carousel.css';
 
 const Carousel = () => {
 
     return (
-        <div className='container-fluid'>
+        <div className="container-fluid">
             <div className="row">
-                <div className=" col-md-12 mb-3 p-0">
+                <div className=" col-md-12 mb-5 p-0">
                     <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                         <div className="carousel-indicators">
                             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -13,25 +14,28 @@ const Carousel = () => {
                             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                         </div>
                         <div className="carousel-inner">
-                            <div className="carousel-item active" data-bs-interval="3000">
+                            <div className="carousel-item active" data-bs-interval="4000">
                                 <img src="images/Danna Blenk/1.jpg" className="d-block w-100" alt="foto artista 1" />
                                 <div className="carousel-caption d-none d-md-block">
-                                    <h1 className="">BIENVENIDOS A MI TIENDA DE ARTE ABSTRACTO</h1>
-                                    <p>Danna Blenk</p>
+                                    <h3 className="texto-carousel"><b>¡BIENVENIDOS A MI TIENDA!</b></h3>
+                                    <p className="texto-dos-carousel">EN DONDE ENCONTRARÁS OBRAS DE ARTE ABSTRACTO</p>
+                                    <p className='nombre-carousel' >Danna Blenk</p>
                                 </div>
                             </div>
-                            <div className="carousel-item" data-bs-interval="3000">
+                            <div className="carousel-item" data-bs-interval="4000">
                                 <img src="images/Danna Blenk/3.jpg" className="d-block w-100" alt="foto artista 3" />
                                 <div className="carousel-caption d-none d-md-block">
-                                    <h1>SE REALIZAN TRABAJOS PERSONALIZADOS</h1>
-                                    <p>Danna Blenk</p>
+                                    <h3 className="texto-carousel"><b>CADA OBRA ES ÚNICA</b></h3>
+                                    <p className="texto-dos-carousel">EL TIEMPO DE ENTREGA ESTIMADO ES DE 10 A 15 DÍAS</p>
+                                    <p className='nombre-carousel'>Danna Blenk</p>
                                 </div>
                             </div>
-                            <div className="carousel-item" data-bs-interval="3000" >
+                            <div className="carousel-item" data-bs-interval="4000" >
                                 <img src="images/Danna Blenk/2.jpg" className="d-block w-100" alt="foto artista 2" />
                                 <div className="carousel-caption d-none d-md-block">
-                                    <h1>REGALÁ Y REGALATE ARTE ORIGINAL</h1>
-                                    <p>Danna Blenk</p>
+                                    <h3 className='texto-carousel'><b>COMPRÁ Y REGALÁ ARTE ORIGINAL</b></h3>
+                                    <p className="texto-dos-carousel">CADA OBRA SE ENTREGA CON SU CERTIFICADO DE AUTENTICIDAD</p>
+                                    <p className='nombre-carousel'>Danna Blenk</p>
                                 </div>
                             </div>
                         </div>
