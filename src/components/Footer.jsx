588bebced3061b84e88458883508026a953233ad
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-04 21:20:05 -0300

    se agregan navlinks e icons + ajustes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import './footer.css';
+import { NavLink } from 'react-router-dom';
+
 
 const Footer = () => {
 
@@ -7,11 +10,10 @@ const Footer = () => {
       <div className="row">
         <div className="col-md-7 px-3 mb-3">
           <h5>Sección</h5>
-          <ul className="nav flex-column">
-            <li className="nav-item"><a href="/inicio" className="nav-link px-0 text-muted">Inicio</a></li>
-            <li className="nav-item"><a href="/obras" className="nav-link px-0 text-muted">Tienda</a></li>
-            <li className="nav-item"><a href="/biografia" className="nav-link px-0 text-muted">Biografía</a></li>
-            <li className="nav-item"><a href="/contacto" className="nav-link px-0 text-muted">Contacto</a></li>
+          <ul className="nav flex-column py-1">
+            <li className="nav-item"><NavLink to={"/"} className="nav-NavLink px-0 text-muted text-decoration-none"><img src={"images/logo e iconos/house.svg"} className="me-2" alt="house" width="18" />Inicio</NavLink ></li>
+            <li className="nav-item"><NavLink to={"/biografia"} className="nav-NavLink px-0 text-muted text-decoration-none"><img src={"images/logo e iconos/person-lines-fill.svg"} className="me-2" alt="person" width="17" />Biografía</NavLink ></li>
+            <li className="nav-item"><NavLink to={"/contacto"} className="nav-NavLink px-0 text-muted text-decoration-none"><img src={"images/logo e iconos/envelope-at.svg"} className="me-2" alt="envelope" width="17" />Contacto</NavLink ></li>
           </ul>
         </div>
 
@@ -21,8 +23,8 @@ const Footer = () => {
             <p>¡Suscribite y recibí todas las novedades!</p>
             <div className="d-flex flex-column flex-sm-row w-100 gap-2">
               <label htmlFor="newsletter1" className="visually-hidden">Dirección de Email</label>
-              <input id="newsletter1" type="text" className="form-control" placeholder="Ingresá tu dirección de Email" />
-              <button className="btn btn-primary" type="button">Suscribirme</button>
+              <input id="newsletter1" type="text" className="form-control" placeholder="Ingresar dirección de Email..." />
+              <button className="btn btn-suscription btn-primary" type="button">Suscribirme</button>
             </div>
           </form>
         </div>
@@ -31,9 +33,9 @@ const Footer = () => {
       <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
         <p className="ms-3">© 2022 By Carina Bravo - All rights reserved.</p>
         <div className="list-unstyled d-flex">
-          <a href="https://www.facebook.com/" className="me-3" ><img src="images/logo/facebook.svg" alt="facebook" width="28" /></a>
-          <a href="https://twitter.com/" className="me-3"  ><img src="images/logo/twitter.svg" alt="twitter" width="28" /></a>
-          <a href="https://www.instagram.com/" className="me-3" ><img src="images/logo/instagram.svg" alt="instagram" width="28" /></a>
+          <a href="https://www.facebook.com/" className="me-3" ><img src="images/logo e iconos/facebook.svg" alt="facebook" width="28" /></a>
+          <a href="https://twitter.com/" className="me-3"  ><img src="images/logo e iconos/twitter.svg" alt="twitter" width="28" /></a>
+          <a href="https://www.instagram.com/" className="me-3" ><img src="images/logo e iconos/instagram.svg" alt="instagram" width="28" /></a>
         </div>
       </div>
     </footer>
