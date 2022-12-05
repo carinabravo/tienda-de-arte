44ff4bb041542cb221d21f88e9f07b43286ea6eb
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-04 21:41:56 -0300

    se crea componente para la información de producto

---
+++ src/components/Item.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './item.css';
+import { Link } from 'react-router-dom';
+
+const Item = ({ item }) => {
+
+    return (
+        <div className="card col-2 ms-3 me-3 mb-5 p-0">
+            <img src={item.image} className="card-img-top" alt="card" />
+            <div className="card-body">
+                <h5 className="card-title">{item.title}</h5>
+                <h6 className="card-price">$ {item.price}</h6>
+                <Link to={`/item/${item.id}`} className="btn btn-card btn-primary">Ver más</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Item;
