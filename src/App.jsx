bdd0fde5d845367aef42760a6161510725c0fade
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-04 21:22:47 -0300

    se agregan rutas a los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,42 @@
 import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Encabezado from "./components/Encabezado";
 import NavBar from "./components/NavBar";
 import Carousel from "./components/Carousel";
-import ItemListContainer from "./components/ItemListContainer";
-import ItemCount from "./components/ItemCount";
 import Promocion from "./components/Promocion";
 import Footer from "./components/Footer";
-import "./App.css";
-
+import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import Error404 from "./components/Error404";
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Encabezado />
       <NavBar />
-      <Carousel />
-      <ItemListContainer greeting={"¡PROXIMAMENTE - CYBER WEEK 2022!"} />
-      <ItemCount initial={1} stock={20} onAdd={() => { }} />
+      <Routes>
+        <Route path={"/"} element={<Inicio />} />
+        <Route path={"/category/:id"} element={<ItemListContainer />} />
+        <Route path={"/item/:id"} element={<ItemDetailContainer />} />
+        <Route path={"*"} element={<Error404 />} />
+      </Routes>
       <Promocion />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 
+}
+
+function Inicio() {
+  // solo se carga el carousel en el inicio
+  return (
+    <>
+      <Carousel />
+      <ItemListContainer />
+    </>
+  )
 }
 
 export default App;
