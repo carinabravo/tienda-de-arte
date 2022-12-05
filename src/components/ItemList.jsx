b02b21653e7517535d367f156472210c978ac9f8
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-05 10:30:34 -0300

    se ajustan medidas

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const ItemList = ({ items }) => {
 
     return (
-        <div className="container mb-5">
+        <div className="container mb-4 mt-3">
             <div className="row d-flex text-center justify-content-center">
                 {items.map(item => <Item key={item.id} item={item} />)}
             </div>
