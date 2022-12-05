0ed4fe608bac5a442a612107e4b2ffb3b40f5330
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-04 21:23:46 -0300

    se realizan ajustes en promoción

--- src/components/Promocion.jsx
+++ src/components/Promocion.jsx
@@ -1,21 +1,23 @@
-import React from "react";
+import React from 'react';
+import './promocion.css';
 
 const Promocion = () => {
+
     return (
-        <div className="container-fluid bg-warning">
+        <div className="container-fluid promo">
             <div className="col-md-12 mb-5">
                 <div className="container">
-                    <div className="row  px-0 py-4">
+                    <div className="text-promo row px-0 py-5">
                         <div className="col-md-4 d-flex justify-content-start align-items-center">
-                            <img src="images/logo/truck.svg" alt="camión de envíos" width="30" />
+                            <img src={"images/logo e iconos/truck.svg"} alt="camión de envíos" width="30" />
                             <p className="m-2">ENVIOS A TODO EL PAÍS</p>
                         </div>
                         <div className="col-md-4 d-flex justify-content-center align-items-center">
-                            <img src="images/logo/credit-card-2-back.svg" alt="tarjeta de crédito" width="30" />
+                            <img src={"images/logo e iconos/credit-card-2-back.svg"} alt="tarjeta de crédito" width="30" />
                             <p className="m-2">PAGÁ COMO QUIERAS</p>
                         </div>
                         <div className="col-md-4 d-flex justify-content-end align-items-center ">
-                            <img src="images/logo/check2-circle.svg" alt="certificado" width="30" />
+                            <img src={"images/logo e iconos/check2-circle.svg"} alt="certificado" width="30" />
                             <p className="m-2">OBRAS ÚNICAS Y CERTIFICADAS</p>
                         </div>
                     </div>
