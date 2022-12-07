5c22c4fbcbe3290afce964d385d0640862318be7
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-07 19:08:10 -0300

    se agrega buscador

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -39,6 +39,10 @@ const NavBar = () => {
                                         </ul>
                                     </li>
                                 </ul>
+                                <form className="d-flex" role="search">
+                                    <input className="form-control me-2" type="search" placeholder="Obra..." aria-label="Search" />
+                                    <button className="btn btn-outline-success" type="submit">Buscar</button>
+                                </form>
                             </div>
                         </div>
                     </nav>
