45b531812a5b0e7fc26eca3f7fffdd4506206ce5
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-18 15:09:16 -0300

    se hace ajuste de medida

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,8 +5,7 @@ import React from 'react';
 const Loader = () => {
 
     return (
-
-        <div className="d-flex justify-content-center py-5">
+        <div className="d-flex justify-content-center py-5 my-5">
             <div className="spinner spinner-border" role="status">
                 <span className="visually-hidden">Cargando...</span>
             </div>
