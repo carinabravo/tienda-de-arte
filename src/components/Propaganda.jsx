6646389d83488eb9094c3b61e13744e72a51589d
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-18 12:29:55 -0300

    se modifican y ajustan texto y medidas

--- src/components/Propaganda.jsx
+++ src/components/Propaganda.jsx
@@ -1,18 +1,18 @@
-import React from "react";
 import './propaganda.css';
+import React from "react";
+
 
 const Propaganda = () => {
     return (
         <div className="container">
-            <div className="row pt-5 pb-3 px-5">
-                <div className="col-md-3 offset-md-1">
-                    <img src="/images/Danna Blenk/propaganda.jpg" alt="" className="img-propaganda" />
+            <div className="row py-4">
+                <div className="col-md-7 offset-md-1 mt-3">
+                    <p className="text-propaganda1">Danna Blenk <b>trabaja</b> en cada pieza poniendo atención a la calidad y el detalle, respetando los procesos de secado de los materiales en su taller.</p>
+                    <p className="text-propaganda2">Realiza <b>envíos</b> a todo el país, cuidando al máximo los detalles de embalaje y empaquetado, utilizando pluribol de alta densidad y cartón corrugado tri-capa para proteger su compra y que usted reciba todo en perfectas condiciones.</p>
+                    <p className="text-propaganda3">El tiempo estimado de <b>entrega</b> de cada producto es de 10 a 15 dìas hábiles y se envían con su certificado de autenticidad firmado por la artista.</p>
                 </div>
-                <div className="col-md-7">
-                    <h5 className="title-propaganda1">TRABAJOS PERSONALIZADOS</h5>
-                    <p className="text-propaganda1 pb-3">Danna trabaja en cada pieza poniendo atenciòn a la calidad y el detalle, respetando los procesos de secado de los materiales en su taller.</p>
-                    <h5 className="title-propaganda2">ENVÍOS</h5>
-                    <p className="text-propaganda2">Realiza envíos a todo el país, por este motivo Danna cuida al máximo los detalles de embalaje y empaquetado de cada obra, utilizando pluribol de alta densidad y cartón corrugado tri-capa para proteger su compra y que usted reciba todo en perfectas condiciones. El tiempo estimado de entrega de la obra es de 10 a 15 dìas aproximadamente. Cada obra se entrega con su certificado de autenticidad.</p>
+                <div className="col-md-2 offset-md-1">
+                    <img src="/images/Danna Blenk/propaganda.jpg" alt="" className="img-propaganda" />
                 </div>
             </div>
             <hr></hr>
