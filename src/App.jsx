fd2dc321f406d8c9fcd0be2e82ef026053e0407b
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-18 12:28:27 -0300

    se agrega context  global

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error404 from "./components/Error404";
 import Encabezado from "./components/Encabezado";
 import NavBar from "./components/NavBar";
 import Carousel from "./components/Carousel";
@@ -10,26 +11,30 @@ import Promocion from "./components/Promocion";
 import Footer from "./components/Footer";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
-import Error404 from "./components/Error404";
 import Biografia from "./components/Biografia";
+import CartContextProvider from "./components/context/CartContext";
+import Cart from "./components/Cart";
 
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Encabezado />
-      <NavBar />
-      <Routes>
-        <Route path={"/"} element={<Inicio />} />
-        <Route path={"/biografia"} element={<Biografia />} />
-        <Route path={"/category/:id"} element={<ItemListContainer />} />
-        <Route path={"/item/:id"} element={<ItemDetailContainer />} />
-        <Route path={"*"} element={<Error404 />} />
-      </Routes>
-      <Promocion />
-      <Footer />
-    </BrowserRouter>
+    <CartContextProvider>
+      <BrowserRouter>
+        <Encabezado />
+        <NavBar />
+        <Routes>
+          <Route path={"/"} element={<Inicio />} />
+          <Route path={"/biografia"} element={<Biografia />} />
+          <Route path={"/category/:id"} element={<ItemListContainer />} />
+          <Route path={"/item/:id"} element={<ItemDetailContainer />} />
+          <Route path={"/cart"} element={<Cart />} />
+          <Route path={"*"} element={<Error404 />} />
+        </Routes>
+        <Promocion />
+        <Footer />
+      </BrowserRouter>
+    </CartContextProvider>
   );
 
 }
