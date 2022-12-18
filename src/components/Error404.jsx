71d13d1772be9e91d3c7cea433cca4bfc9b1a74a
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-18 15:08:26 -0300

    se modifica boton

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -10,7 +10,7 @@ const Error404 = () => {
                 <div className="text-center py-1" role="alert">
                     <img src="./images/logo e iconos/error.jpg" className="img-error me-4" alt="" />
                     <h4 className="text-error">Oops! Page not found</h4>
-                    <Link className="btn btn-error btn-primary mt-2 mb-5" to={"/"} type="button">Go Home</Link>
+                    <Link to={"/"} className="btn btn-error btn-primary mt-2 mb-5">Go Home</Link>
                 </div>
             </div>
         </div>
