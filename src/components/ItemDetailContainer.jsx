ca3301605a4c50dfe7b8d872bb75f6ac997389aa
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-21 18:51:44 -0300

    Se remplazan promesas por llamados de Firestore

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import arrayProductos from './json/products.json';
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 import Loader from './Loader';
 import ItemDetail from "./ItemDetail";
-
+// import arrayProductos from './json/products.json';
 
 
 const ItemDetailContainer = () => {
@@ -11,19 +11,36 @@ const ItemDetailContainer = () => {
     const { id } = useParams();
     const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        const promesa = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                //filtra el array de productos y devuelve un solo objeto.
-                resolve(arrayProductos.find(item => item.id === parseInt(id)));
-            }, 2000);
+    // useEffect(() => {
+    //     const promesa = new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             //filtra el array de productos y devuelve un solo objeto.
+    //             resolve(arrayProductos.find(item => item.id === parseInt(id)));
+    //         }, 2000);
 
+    //     });
+
+    //     promesa.then((data) => {
+    //         setLoading(false)
+    //         setItem(data);
+    //     })
+    // }, [id]);
+
+
+    //Consulta a un documento pasado por ID
+    useEffect(() => {
+        const db = getFirestore();
+        const item = doc(db, "items", id);
+
+        getDoc(item).then((datos) => {
+            if (datos.exists()) {
+                setItem({ id: datos.id, ...datos.data() });
+            } else {
+                // console.log("el producto no existe!");
+            }
+            setLoading(false)
         });
 
-        promesa.then((data) => {
-            setLoading(false)
-            setItem(data);
-        })
     }, [id]);
 
 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,29 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
-import arrayProductos from './json/products.json';
+import { collection, getDocs, getFirestore, where, query } from 'firebase/firestore';
 import Loader from './Loader';
 import ItemList from './ItemList';
+import { useParams } from "react-router-dom";
+// import arrayProductos from './json/products.json';
+
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([])
-    const { id: category } = useParams();
     const [loading, setLoading] = useState(true)
+    const { id: category } = useParams();
 
+    // useEffect(() => {
+    //     setLoading(true);
+
+    //     const promesa = new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             // filtra por categoría de productos sino muestra todos los productos.
+    //             resolve(category ? arrayProductos.filter(item => item.category === category) : arrayProductos);
+    //         }, 2000);
+
+    //     });
+
+    //     promesa.then((data) => {
+    //         setLoading(false)
+    //         setItems(data);
+    //     })
+    // }, [category]);
+
+
+    //Proceso para cargar productos de la colecciòn
+    // useEffect(() => {
+    //     const db = getFirestore();
+    //     const itemsCollection = collection(db, "items");
+
+    //     arrayProductos.forEach((item) => {
+    //         addDoc(itemsCollection, { title: item.title, article: item.article, description: item.description, price: item.price, image: item.image, stock: item.stock, category: item.category });
+    //     });
+    // }, []);
+
+
+    //Se consulta a la collecciòn de datos
     useEffect(() => {
         setLoading(true);
+        const db = getFirestore();
+        //Se filtra por categorías con uso de ternario
+        const itemsCollection = category
+            ? query(collection(db, 'items'), where("category", "==", category))
+            : collection(db, "items");
 
-        const promesa = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                // filtra por categoría de productos sino muestra todos los productos.
-                resolve(category ? arrayProductos.filter(item => item.category === category) : arrayProductos);
-            }, 2000);
-
+        getDocs(itemsCollection).then((datos) => {
+            setItems(datos.docs.map((doc) => ({ id: doc.id, ...doc.data() })))
+            setLoading(false)
         });
 
-        promesa.then((data) => {
-            setLoading(false)
-            setItems(data);
-        })
     }, [category]);
 
 
