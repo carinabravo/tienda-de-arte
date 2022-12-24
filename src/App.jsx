ed5ceaf611e8cb57fa079140018e8dec02a2f889
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-23 23:41:04 -0300

    se agrega componente checkout

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ItemDetailContainer from "./components/ItemDetailContainer";
 import Biografia from "./components/Biografia";
 import CartContextProvider from "./components/context/CartContext";
 import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
           <Route path={"/category/:id"} element={<ItemListContainer />} />
           <Route path={"/item/:id"} element={<ItemDetailContainer />} />
           <Route path={"/cart"} element={<Cart />} />
+          <Route path={"/checkout"} element={<Checkout />} />
           <Route path={"*"} element={<Error404 />} />
         </Routes>
         <Promocion />
