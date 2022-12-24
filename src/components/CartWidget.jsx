834b40869ef90365529ac5f079f372e59ca8604c
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2022-12-23 23:47:55 -0300

    se hacen ajustes de medidas en carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -6,10 +6,9 @@ const CartWidget = () => {
     const { cartTotal } = useContext(CartContext);
     return (
         <Link to={"/cart"} className="btn position-relative" >
-            <img src={"images/logo e iconos/cart4.svg"} alt="Carrito" width="28" />
+            <img src={"images/logo e iconos/cart4.svg"} alt="Carrito" width="25" />
             <span className="position-absolute start-5 translate-middle badge rounded-pill bg-danger">{cartTotal()}</span>
         </Link>
-
     )
 }
 
