d5841e27b3920980f2db3606d76f1de6d5808db7
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2023-01-05 20:47:45 -0300

    se crea componente

---
+++ src/components/TituloPrincipal.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const TituloPrincipal = () => {
+    return (
+        <div className="container">
+            <div className="col-md-12">
+                <div className="row text-center px-5">
+                    <p className="mt-4 mb-1" style={{fontSize:"28px"}}>Te presento mi nueva Serie titulada <b>"Vida"</b></p>
+                    <p>Una collección única de <b>Arte abstracto</b>, la cual se encuentra conformada por 16 magníficas Obras.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TituloPrincipal;
