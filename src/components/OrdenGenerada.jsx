7729705fbdec81ff10b3ee5c3555b13dcdf7d1c5
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2023-01-05 17:10:41 -0300

    se modifica texto

--- src/components/OrdenGenerada.jsx
+++ src/components/OrdenGenerada.jsx
@@ -10,8 +10,8 @@ const OrdenGenerada = () => {
             <div className="row py-5 px-5">
                 <div className="col-md-12 text-center mb-5">
                     <div className="alert alert-success">
-                        <h1 className="mb-2" style={{ fontSize: "25px" }}>Agradecemos su compra!</h1>
-                        <p className="mb-0"style={{ fontSize: "15px" }}>Su número de orden es: <b>{id}</b></p>
+                        <h1 className="mb-3" style={{ fontSize: "25px" }}>Agradecemos tu compra!</h1>
+                        <p className="mb-0"style={{ fontSize: "16px" }}>Número de orden: <b>{id}</b> </p>
                     </div>
                     <Link to={"/"} className="btn btn-volver btn-primary mt-2">Volver a la página principal</Link>
                 </div>
