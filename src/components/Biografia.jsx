e567f21ab3c7fd1786bdac2db555805f31d1267e
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2023-01-05 17:05:21 -0300

    se acomoda texto

--- src/components/Biografia.jsx
+++ src/components/Biografia.jsx
@@ -13,11 +13,19 @@ const Biografia = () => {
                 </div>
                 <div className="col-md-5">
                     <h1 className="title-perfil">Danna Blenk</h1>
-                    <p className="text-perfil">Nació en un pequeño pueblo de Londres, donde desde muy temprana edad mostró su pasión por el arte. A los 18 años decidió mudarse a Buenos Aires para estudiar en la prestigiosa Escuela de Bellas Arte, donde se especializó en el arte abstracto.
-                        Desde entonces, Danna ha expuesto sus obras en diversos lugares de Argentina, siendo reconocida por su habilidad para crear piezas y texturas que juegan con los colores y las formas, siempre en busca de transmitir emociones y sensaciones a través de su maravilloso arte.
-                        Su trabajo ha sido galardonado en numerosas ocasiones, y ha sido invitada a participar en ferias y festivales de arte en distintos países, lo que le ha permitido consolidarse como una de las artistas más destacadas de su generación.
-                        Danna siempre ha seguido su corazón y su pasión por el arte abstracto, convirtiéndose en una figura importante en la escena artística.
-                        Su talento y dedicación le han permitido alcanzar el éxito y la admiración de muchos, siendo así una gran fuente de inspiración para aquellos que aman el arte.</p>
+                    <p className="text-perfil">Nació en un pequeño pueblo de Londres, donde desde muy temprana edad mostró su pasión
+                        por el arte. A los 18 años decidió mudarse a Buenos Aires para estudiar en la prestigiosa Escuela de Bellas Arte,
+                        donde se especializó en el arte abstracto.
+                        Desde entonces, Danna ha expuesto sus obras en diversos lugares de Argentina, siendo reconocida por su habilidad
+                        para crear piezas y texturas que juegan con los colores y las formas, siempre en busca de transmitir emociones y
+                        sensaciones a través de su maravilloso arte.
+                        Su trabajo ha sido galardonado en numerosas ocasiones, y ha sido invitada a participar en ferias y festivales de
+                        arte en distintos países, lo que le ha permitido consolidarse como una de las artistas más destacadas de su
+                        generación.
+                        Danna siempre ha seguido su corazón y su pasión por el arte abstracto, convirtiéndose en una figura importante
+                        en la escena artística.
+                        Su talento y dedicación le han permitido alcanzar el éxito y la admiración de muchos, siendo así una gran fuente
+                        de inspiración para aquellos que aman el arte.</p>
                 </div>
                 <div>
                     <button className="btn me-5" type="submit" onClick={() => navigate(-1)}>
