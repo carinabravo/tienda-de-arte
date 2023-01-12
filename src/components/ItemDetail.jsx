c3a3b3fe84674c660eba3305ef015c00a9fc8bb8
Carina Bravo <54654136+carinabravo@users.noreply.github.com>
2023-01-11 22:49:47 -0300

    se realizan ajustes de medida

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -22,7 +22,7 @@ const ItemDetail = ({ item }) => {
     return (
         <div className="row">
             <div className="col-lg-5 col-md-5 col-sm-12 offset-md-1">
-                <img src={item.image} alt={item.title} className="img-unica img-fluid mb-3" />
+                <img src={item.image} alt={item.title} className="img-unica img-fluid mb-4" />
             </div>
             <div className="col-lg-5 col-md-5 col-sm-12">
                 <h1 className="item-title">{item.title}</h1>
